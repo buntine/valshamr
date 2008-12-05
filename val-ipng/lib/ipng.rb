cb6756bda2195bb9e52d37934af2d0b47c4093fa
Andy Buntine <info@andrewbuntine.com>
2008-12-05 22:23:20 +1100

    Stubbed out the general API

--- val-ipng/lib/ipng.rb
+++ val-ipng/lib/ipng.rb
@@ -7,8 +7,31 @@ class Valshamr::IPng
   end
 
   def transform!
+    unless is_valid_ip?
+      raise Valshamr::InvalidIPError "Expected IPv4 address in the form of xxx.xxx.xxx.xxx, but received #{@ip_address}"
+    end
+
+    octets = @ip_address.split "."
+    octets.map! { |octet| convert_octet_to_hexadecimal octet }
+
+    "::#{octets.join(':')}"
+  end
+
+  private
+
+  def is_valid_ip?
+    true
+  end
+
+  def is_valid_octet?(octet)
+    true
+  end
+
+  def convert_octet_to_hexadecimal(octet)
+    "FF"
   end
 
 end
 
 class Valshamr::InvalidIPError < Exception; end
+class Valshamr::InvalidOctetError < Exception; end
