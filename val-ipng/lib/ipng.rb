06be055dc4c3ff6a1206641f0a6d427df504b4ca
Andy Buntine <info@andrewbuntine.com>
2008-12-06 15:31:44 +1100

    Added method to IPng address is formatted correctly

--- val-ipng/lib/ipng.rb
+++ val-ipng/lib/ipng.rb
@@ -7,32 +7,44 @@ class Valshamr::IPng
   end
 
   def transform!
-    unless is_valid_address?
+    unless is_valid_ipv4_address?
       raise Valshamr::InvalidIPError, "Expected IPv4 address in the form of x[xx].x[xx].x[xx].x[xx], but received: #{@ip_address}"
     end
 
     octets = @ip_address.split "."
-    octets.map! { |octet| convert_octet_to_hexadecimal octet }
+    octets.map! { |octet| convert_decimal_octet_to_hexadecimal octet }
 
-    "::#{octets.join(':')}"
+    new_ip = construct_hexadecimal_octets octets
+
+    "::#{new_ip}"
   end
 
   private
 
-  def is_valid_address?
+  def is_valid_ipv4_address?
     @ip_address =~ /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/
   end
 
-  def is_valid_octet?(octet)
+  def is_valid_decimal_octet?(octet)
     (0..255).include? octet.to_i
   end
 
-  def convert_octet_to_hexadecimal(octet)
-    unless is_valid_octet? octet
+  def convert_decimal_octet_to_hexadecimal(octet)
+    unless is_valid_decimal_octet? octet
       raise Valshamr::InvalidOctetError, "Expected octet in the range of 0..255, but received: #{octet}"
     end
 
     "FF"
+  end
+
+  def construct_hexadecimal_octets(octets)
+    octet_group = []
+
+    octets.each_slice(2) do |slice|
+      octet_group << slice.join
+    end
+
+    octet_group.join ":"
   end
 
 end
