06750870a0ce1097131091fa4bc2648cc7fb0d8c
Andy Buntine <info@andrewbuntine.com>
2008-12-07 20:47:05 +1100

    Added IPng and Expand scripts

---
+++ script/expand.rb
@@ -0,0 +1,35 @@
+#! /usr/bin/env ruby
+
+require File.join(File.dirname(__FILE__), '../lib/valshamr')
+require 'fcntl'
+require 'optparse'
+
+length = :short
+
+# Parse the supplied options.
+OptionParser.new do |opts|
+  opts.banner = "Usage: expand.rb [options] [address(es)]"
+
+  opts.on("-e", "--expand", "Fully expand the address") do
+    length = :long
+  end
+end.parse!
+
+addresses = ARGV
+
+# Read in from stdin if there is something there.
+if STDIN.fcntl(Fcntl::F_GETFL, 0) == 0
+  addresses.concat STDIN.readlines.map { |line| line.sub /\n$/, '' }
+end
+
+# Expand each given IP address.
+addresses.each do |ip_address|
+  expander = Valshamr::Expand.new ip_address
+
+  begin
+    puts expander.expand(length)
+  rescue => e
+     exit 1
+     raise e
+  end
+end
---
+++ script/ipng.rb
@@ -0,0 +1,29 @@
+#! /usr/bin/env ruby
+
+require File.join(File.dirname(__FILE__), '../lib/valshamr')
+require 'fcntl'
+require 'optparse'
+
+# Supplies a help massage on -h/--help.
+OptionParser.new do |opts|
+  opts.banner = "Usage: ipng.rb [address(es)]"
+end.parse!
+
+addresses = ARGV
+
+# Read in from stdin if there is something there.
+if STDIN.fcntl(Fcntl::F_GETFL, 0) == 0
+  addresses.concat STDIN.readlines.map { |line| line.sub /\n$/, '' }
+end
+
+# Convert each given IP address.
+addresses.each do |ip_address|
+  ipng = Valshamr::IPng.new ip_address
+
+  begin
+    puts ipng.transform
+  rescue => e
+     exit 1
+     raise e
+  end
+end
