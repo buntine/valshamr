760d175167a79a6340d69313dbbc0a36ada1c115
Andy Buntine <info@andrewbuntine.com>
2008-12-14 23:26:23 +1100

    Added to_binary script

---
+++ script/to_binary.rb
@@ -0,0 +1,38 @@
+#! /usr/bin/env ruby
+
+require File.join(File.dirname(__FILE__), '../lib/valshamr')
+require 'fcntl'
+require 'optparse'
+
+bits_per_line = 32
+shown = false
+
+# Parse the supplied options.
+OptionParser.new do |opts|
+  opts.banner = "Usage: to_binary.rb [options] [address(es)]"
+
+  opts.on("-b", "--bits N", "Display the data with N bits per line") do |bits|
+    bits_per_line = bits.to_i
+  end
+end.parse!
+
+addresses = ARGV
+
+# Read in from stdin if there is something there.
+if STDIN.fcntl(Fcntl::F_GETFL, 0) == 0
+  addresses.concat STDIN.readlines.map { |line| line.sub /\n$/, '' }
+end
+
+#  each given IP address.
+addresses.each do |ip_address|
+  to_binary = Valshamr::ToBinary.new ip_address
+
+  begin
+    puts to_binary.transform(bits_per_line)
+    puts "-" * 10 if shown
+    shown = true
+  rescue Valshamr::InvalidIPv6Error, Valshamr::InvalidBitCount
+     puts "ERROR: #{$!.message}"
+     exit 1
+  end
+end
